886ffd3487b0aa6ea833bf8b7be105c5b15f07f4
Iris-Grimpampi <ice21390045@uniwa.gr>
2024-10-21 22:15:15 +0300

    idk man

--- EShopAsk/src/eshopask/EShopAsk.java
+++ EShopAsk/src/eshopask/EShopAsk.java
@@ -14,7 +14,7 @@ public class EShopAsk {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hi!!");   
+        System.out.println("Hi!!");     
         
         // TODO code application logic here
     }
